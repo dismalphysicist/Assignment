f36b5aad24810b58d34e0292162c15a127dda411
dismalphysicist <46093766+dismalphysicist@users.noreply.github.com>
2018-12-24 12:09:48 +0000

    Nearly fixed

--- Assignment/routes/index.js
+++ Assignment/routes/index.js
@@ -1,22 +1,28 @@
 ﻿'use strict';
 var express = require('express');
 var router = express.Router();
-var app = express();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
 
 var people = [{ "username": "doctorwhocomposer", "forename": "Delia", "surname": "Derbyshire" }];
 var numbers = [1, 2, 3];
 
-app.get("/numbers", function (req, resp) {
+router.get("/numbers", function (req, resp) {
     const n = req.query.number;
     console.log("communicated");
     resp.send("hello world " + n);
 })
 
-app.get("/allnumbers", function (req, resp) {
+router.get("/allnumbers", function (req, resp) {
     resp.send("All numbers: " + numbers);
 })
 
-app.post('/addnumber', function (req, resp) {
+router.post('/addnumber', function (req, resp) {
 
     const t = req.body.number;
     numbers.push(t);
