65a93b8e77494a91f0140040946c5dd69bd31e47
dismalphysicist <46093766+dismalphysicist@users.noreply.github.com>
2018-12-24 11:52:27 +0000

    Put in server side scripting

--- Assignment/routes/index.js
+++ Assignment/routes/index.js
@@ -8,3 +8,25 @@ router.get('/', function (req, res) {
 });
 
 module.exports = router;
+
+var people = [{ "username": "doctorwhocomposer", "forename": "Delia", "surname": "Derbyshire" }];
+var numbers = [1, 2, 3];
+
+app.get("/numbers", function (req, resp) {
+    const n = req.query.number;
+    console.log("communicated");
+    resp.send("hello world " + n);
+})
+
+app.get("/allnumbers", function (req, resp) {
+    resp.send("All numbers: " + numbers);
+})
+
+app.post('/addnumber', function (req, resp) {
+
+    const t = req.body.number;
+    numbers.push(t);
+    resp.send("number added " + t);
+})
+
+app.listen(8090);
